9a7af489e4bea2f95556100c3d063c4e38b7c9d9
pedro <pvmonteiro26@gmail.com>
2020-07-18 16:24:12 -0300

    feat: edit and destroy action

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,21 @@
 class Admin::UsersController < AdminController
+
+  before_action :set_user, only: [:edit, :update, :destroy]
+
   def index
     @users = User.includes(:pre_registration)
   end
+
   def new
     @user = User.new
   end
+
   def create
     #@user = User.new(user_params)
     @user = User.create_with_password(user_params)
     respond_to do |format|
       if @user.save
-        format.html { redirect_to admin_users_path, notice: 'Pre registration was successfully created.' }
+        format.html { redirect_to admin_users_path, notice: 'User was successfully created.' }
           format.json { render :show, status: :created, location: @user }
         else
           format.html { render :new }
@@ -19,9 +24,31 @@ class Admin::UsersController < AdminController
       end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to admin_users_path, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_users_path, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
   private
   def set_user
-    @use = User.find(params[:id])
+    @user = User.find(params[:id])
   end
   def user_params
     params.require(:user).permit(:user_name, :email, :password, :password_confirmation, :pre_registration_id, :role)

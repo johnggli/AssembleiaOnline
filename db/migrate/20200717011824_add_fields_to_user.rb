395d1cf8b96d47af0dc7d197fe539393e5f3e692
JohnEmerson1406 <john.carv.sousa@gmail.com>
2020-07-18 19:20:37 -0300

    refactor: remove arrow of user migration

--- db/migrate/20200717011824_add_fields_to_user.rb
+++ db/migrate/20200717011824_add_fields_to_user.rb
@@ -2,7 +2,7 @@ class AddFieldsToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :user_name, :string
     add_column :users, :role, :integer
-    add_column :users, :paid, :boolean, :default => true
+    add_column :users, :paid, :boolean, default: :true
     add_reference :users, :pre_registration, null: false, foreign_key: true
   end
 end

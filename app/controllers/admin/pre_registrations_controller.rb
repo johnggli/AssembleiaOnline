35f379ef29866b91df76f501de2e05afaff8720e
JohnEmerson1406 <john.carv.sousa@gmail.com>
2020-07-20 16:20:20 -0300

    refactor: change to before_action in pre_registrations

--- app/controllers/admin/pre_registrations_controller.rb
+++ app/controllers/admin/pre_registrations_controller.rb
@@ -1,5 +1,6 @@
 class Admin::PreRegistrationsController < AdminController
   before_action :set_pre_registration, only: [:show, :edit, :update, :destroy]
+  before_action :set_apartments, only: [:new, :create, :edit, :update]
 
 
   # GET /pre_registrations
@@ -16,19 +17,16 @@ class Admin::PreRegistrationsController < AdminController
   # GET /pre_registrations/new
   def new
     @pre_registration = PreRegistration.new
-    @apartments = set_apartments
   end
 
   # GET /pre_registrations/1/edit
   def edit
-    @apartments = set_apartments
   end
 
   # POST /pre_registrations
   # POST /pre_registrations.json
   def create
     @pre_registration = PreRegistration.new(pre_registration_params)
-    @apartments = set_apartments
 
     respond_to do |format|
     if @pre_registration.save
@@ -44,7 +42,6 @@ class Admin::PreRegistrationsController < AdminController
   # PATCH/PUT /pre_registrations/1
   # PATCH/PUT /pre_registrations/1.json
   def update
-    @apartments = set_apartments
     respond_to do |format|
       if @pre_registration.update(pre_registration_params)
         format.html { redirect_to admin_pre_registrations_path, notice: 'Pre registration was successfully updated.' }
@@ -68,11 +65,11 @@ class Admin::PreRegistrationsController < AdminController
 
   private
     def set_apartments
-      [101, 102, 103, 104] +
-      [201, 202, 203, 204] +
-      [301, 302, 303, 304] +
-      [401, 402, 403, 404] +
-      [501, 502, 503, 504]
+      @apartments = [101, 102, 103, 104] +
+                    [201, 202, 203, 204] +
+                    [301, 302, 303, 304] +
+                    [401, 402, 403, 404] +
+                    [501, 502, 503, 504]
     end
 
     # Use callbacks to share common setup or constraints between actions.

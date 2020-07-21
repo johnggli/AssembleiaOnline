8875f6e444aa1ad7ba73ae82b8b61180ad68f603
pedro <pvmonteiro26@gmail.com>
2020-07-21 19:12:34 -0300

    feat: Add bootstrap to pre registration and users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,8 +15,9 @@ class Admin::UsersController < AdminController
     @user = User.create_with_password(user_params)
     respond_to do |format|
       if @user.save
-        format.html { redirect_to admin_users_path, notice: 'User was successfully created.' }
-          format.json { render :show, status: :created, location: @user }
+        flash[:success] = "User was successfully created."
+        format.html { redirect_to admin_users_path }
+        format.json { render :show, status: :created, location: @user }
         else
           format.html { render :new }
           format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -30,7 +31,8 @@ class Admin::UsersController < AdminController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to admin_users_path, notice: 'User was successfully updated.' }
+        flash[:success] = "User was successfully update."
+        format.html { redirect_to admin_users_path }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -42,7 +44,8 @@ class Admin::UsersController < AdminController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to admin_users_path, notice: 'User was successfully destroyed.' }
+      flash[:success] = "User was successfully destroy."
+      format.html { redirect_to admin_users_path }
       format.json { head :no_content }
     end
   end

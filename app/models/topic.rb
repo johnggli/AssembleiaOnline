194f0b9c3355a0255110064ff824a33dba34d1e0
JohnEmerson1406 <john.carv.sousa@gmail.com>
2020-07-22 21:21:30 -0300

    feat: add cards to topic show

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -18,6 +18,8 @@
 #  fk_rails_...  (assembly_id => assemblies.id)
 #
 class Topic < ApplicationRecord
+  default_scope { order(id: :desc) }
+
   belongs_to :assembly, inverse_of: :topics
 
   has_rich_text :description
@@ -26,6 +28,4 @@ class Topic < ApplicationRecord
 
   accepts_nested_attributes_for :options, reject_if: :all_blank, allow_destroy: true
 
-  # validates :title, presence: true
-  #validates :description, presence: true
 end

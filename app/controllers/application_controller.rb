1d64a8ee8852c784b1f4a78b96e26d49411aec34
pedro <pvmonteiro26@gmail.com>
2020-07-23 16:50:12 -0300

    Feat: view User home and authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller? 
+ 
+    protected 
+ 
+   #Para permitir a atualização de um atributo "current_locale" que adicionei ao model
+   def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :pre_registration_id]) 
+   end
 end

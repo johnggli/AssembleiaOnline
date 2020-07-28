224735b9bce5277c61ae40762a0c6894dd0e56f5
pedro <pvmonteiro26@gmail.com>
2020-07-28 19:17:08 -0300

    feat: add partial votes

--- app/controllers/assemblies_controller.rb
+++ app/controllers/assemblies_controller.rb
@@ -13,10 +13,9 @@ class AssembliesController < ApplicationController
   end
 
   def do_a_vote
-    Vote.create!(user_id: current_user.id, option_id: params[:id])
-    
-    @assembly = Option.find(params[:id]).topic.assembly
-    redirect_to assembly_path(@assembly)
+    Vote.create!(user_id: current_user.id, option_id: params[:option_id])
+    @topic_render = Option.find(params[:option_id]).topic
+    @votes_count = @topic_render.options.map { |option| option.votes.count }.sum
   end
 
   def history_votes
